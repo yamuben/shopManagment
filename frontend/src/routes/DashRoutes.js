9b7ebe239a99089d21513023940dcac865c71066
Ben <yamubbenjamin@gmail.com>
2023-01-31 09:28:16 +0200

    last push

--- frontend/src/routes/DashRoutes.js
+++ frontend/src/routes/DashRoutes.js
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import DashLayout from "../components/Dashlayout";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 const DashRoutes = () => {
   let  token= true;
   // const {token} =useSelector((state) => state.auth)
